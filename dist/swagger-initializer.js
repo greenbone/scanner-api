04fcf7681c19257bc65be5d7bc90ccb513a3f4e7
Kraemii <c.kraemer.rulle@web.de>
2026-05-13 10:22:38 +0200

    New openapi specification

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    urls: [{ url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/main/rust/doc/openapi.yml", name: "Scanner API" },{ url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/main/rust/doc/reverse-sensor-openapi.yml", name: "Reverse Scanner API" }],"urls.primaryName": "Scanner API",
+    urls: [{ url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/c510747615140756adaa09f4968d3038ad0d5f69/rust/api/openapi.yml", name: "Scanner API" },{ url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/main/rust/doc/reverse-sensor-openapi.yml", name: "Reverse Scanner API" }],"urls.primaryName": "Scanner API",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
